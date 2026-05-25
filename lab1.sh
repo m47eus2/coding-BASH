c4035bd9a4a3dbedfdd3372d447c3c3cc09c912a
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-05-25 12:35:35 +0200

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,28 @@
+2. id
+3. who
+4. man 7 hier
+5. ls ~
+6. ls /dev
+7. mkdir kat1
+8. mkdir -p kat2/kat3/kat4
+9. rm -r kat2/kat3
+10. touch t1.txt t2.txt t3.txt c1.c c2.c c3.c
+11. cp *.txt kat1
+12. cp * .c kat1/kat2
+13. cp -r kat1 kat1b
+14. rm kat1/kat2/*
+15. rm -r kat1b
+16. mv kat1/t1.txt kat1/t4.txt
+17. mv kat1/kat2 kat2b
+18. find /usr -type f -name "*mozilla*"
+19. find /usr -type d -name "bin"
+20. find /usr/bin -type f -size +10c -size -100c -exec cp {} ~/kat \;
+21. touch plik
+22. chmod o+w plik
+23. chmod u-w plik
+24. chmod a+x plik
+25. chmod a=r plik
+26. chmod 664 plik
+27. ln plik plik2
+28. ln -s kat1/kat2 abc
+29. man...
