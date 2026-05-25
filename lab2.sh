fa5379b96950fc4733ccd53d360dd5afc505549b
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-05-25 13:25:57 +0200

    Create lab2.sh

---
+++ lab2.sh
@@ -0,0 +1,13 @@
+4. top -o %CPU
+5. ps -l
+6. kill -15
+7. 
+ps aux | grep firefox
+kill -19 11889 / pkill -19 firefox
+kill -18 11889 / pkill -18 firefox
+8. 
+mkdir readonly
+chmod -w readonly/
+touch readonly/plik || echo ERROR
+
+
